0881195dffc073910b793c22d79755d6ed1c605e
Santiago Garcia <santiago.garcia@posteo.de>
2025-07-30 14:47:43 +0200

    Finish usePreferredLanguage

--- src/hooks/usePreferredLanguage/usePreferredLanguage.ts
+++ src/hooks/usePreferredLanguage/usePreferredLanguage.ts
@@ -1,3 +1,26 @@
+import { useSyncExternalStore } from "react"
+
+function checkForWindow() {
+    if (typeof window === "undefined") {
+        throw new Error("window undefined. Is this running on the server?")
+    }
+
+}
+
+function subscribe(callback: () => void) {
+    checkForWindow()
+    window.addEventListener('languagechange', callback)
+
+    return () => window.removeEventListener('languagechange', callback)
+}
+
+
+function getSnapshot(): string {
+    checkForWindow()
+    return window.navigator.language
+}
+
 export default function usePreferredLanguage() {
-    return "zh-HK";
+    const language = useSyncExternalStore(subscribe, getSnapshot)
+    return language
 }
