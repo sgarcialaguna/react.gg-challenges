492be2804bdd98321642ab46cecaf8861080519d
Santiago Garcia <santiago.garcia@posteo.de>
2025-07-30 11:45:52 +0200

    Initial commit, useDocumentTitle

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./theme.css";
+import "./styles.css";
+
+import UseDocumentTitle from "./sections/UseDocumentTitle";
+
+function App() {
+  return (
+    <>
+      <UseDocumentTitle></UseDocumentTitle>
+    </>
+  );
+}
+
+export default App;
---
+++ src/sections/UseDocumentTitle.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+export default function UseDocumentTitle() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => setCount(count + 1);
+
+  useDocumentTitle(`Clicked ${count} times.`);
+
+  return (
+    <section>
+      <h1>useDocumentTitle</h1>
+      <button className="primary" onClick={handleClick}>
+        Increment Count: {count}
+      </button>
+    </section>
+  );
+}
