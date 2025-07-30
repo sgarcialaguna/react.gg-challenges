64dfc5d93a3985635f3d8e7a8f369fe03d76d118
Santiago Garcia <santiago.garcia@posteo.de>
2025-07-30 14:07:28 +0200

    Fix initial navigation

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,19 +1,22 @@
-import { Outlet, useNavigate } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 
 export default function Root() {
   const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
 
   return (
     <>
       <select
         style={{ marginInline: "auto", marginTop: "2rem" }}
         onChange={(event) => navigate(event.target.value)}
+        defaultValue={location.pathname}
       >
         <option value="/">Navigate to a hook</option>
-        <option value="useDocumentTitle">useDocumentTitle</option>
-        <option value="useDefault">useDefault</option>
-        <option value="useToggle">useToggle</option>
-        <option value="usePrevious">usePrevious</option>
+        <option value="/useDocumentTitle">useDocumentTitle</option>
+        <option value="/useDefault">useDefault</option>
+        <option value="/useToggle">useToggle</option>
+        <option value="/usePrevious">usePrevious</option>
       </select>
       <Outlet></Outlet>
     </>
