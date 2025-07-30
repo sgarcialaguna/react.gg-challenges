0ccd3292ebf9730aa62bd9d36cdd4824b1b3ad30
Santiago Garcia <santiago.garcia@posteo.de>
2025-07-30 15:08:19 +0200

    Implement useFavicon

--- src/hooks/useFavicon/useFavicon.ts
+++ src/hooks/useFavicon/useFavicon.ts
@@ -1,3 +1,14 @@
-import * as React from "react";
+import { useEffect } from "react"
 
-export default function useFavicon() { }
+export default function useFavicon(url: string) {
+    useEffect(() => {
+        let element = document.querySelector<HTMLLinkElement>('link[rel="icon"]')
+        if (!element) {
+            element = document.createElement('link')
+            element.rel = 'icon'
+            document.head.appendChild(element)
+        }
+        element.href = url
+
+    }, [url])
+}
