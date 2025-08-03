b6a723cb596efb3406ac58e17bb23e77742da7f3
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-03 17:49:08 +0200

    useLockBodyScroll solution

--- src/hooks/useLockBodyScroll/useLockBodyScroll.ts
+++ src/hooks/useLockBodyScroll/useLockBodyScroll.ts
@@ -1,1 +1,9 @@
-export default function useLockBodyScroll() { }
+import { useEffect } from "react";
+
+export default function useLockBodyScroll() {
+    useEffect(() => {
+        const oldValue = document.body.style.overflow
+        document.body.style.overflow = 'hidden'
+        return () => { document.body.style.overflow = oldValue }
+    }, [])
+}
