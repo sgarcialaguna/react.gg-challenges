75a4d32372c6c51cd2b2b5a0ec24a5c263b4edf3
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-03 17:25:37 +0200

    Correctly install experimental version of React and complete useInterval

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/// <reference types="react/experimental" />
 import "./theme.css";
 
 import Root from "./Root";
--- src/hooks/useInterval/useInterval.ts
+++ src/hooks/useInterval/useInterval.ts
@@ -1,5 +1,24 @@
-import { useEffectEvent } from 'react'
+import { useEffect, useRef, useState, experimental_useEffectEvent as useEffectEvent } from 'react'
 
-export default function useInterval(cb, ms) {
-    return () => { };
+
+
+export default function useInterval(cb: () => void, ms: number) {
+    const interval = useRef<number>(undefined)
+    const [stopped, setStopped] = useState(false)
+
+    const invokeCallback = useEffectEvent(cb)
+
+    useEffect(() => {
+        console.log("FIRING EFFECT")
+        interval.current = window.setInterval(invokeCallback, ms)
+        return () => window.clearInterval(interval.current)
+    }, [invokeCallback, ms])
+
+    useEffect(() => {
+        if (stopped) {
+            window.clearInterval(interval.current)
+        }
+    }, [stopped])
+
+    return () => setStopped(true)
 }
