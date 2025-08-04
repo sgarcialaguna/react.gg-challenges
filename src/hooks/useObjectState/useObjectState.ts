b0ce9a65f90f43548fa6b985c291a636ea87ca7a
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-04 15:17:10 +0200

    Complete useObjectState

--- src/hooks/useObjectState/useObjectState.ts
+++ src/hooks/useObjectState/useObjectState.ts
@@ -1,9 +1,18 @@
-import * as React from "react";
+import { useState, type SetStateAction } from "react";
 
-const isPlainObject = (value) => {
+const isPlainObject = (value: unknown) => {
     return Object.prototype.toString.call(value) === "[object Object]";
 };
 
-export default function useObjectState(initialValue) {
-    return [{}, () => { }];
+export default function useObjectState<T>(initialValue: Record<string, T>): [Record<string, T>, (arg0: Record<string, T> | SetStateAction<Record<string, T>>) => void] {
+    const [value, setValue] = useState(initialValue)
+    return [value, (newValue) => {
+        if (isPlainObject(newValue)) {
+            return setValue(v => ({ ...v, ...newValue }))
+        }
+        if (typeof newValue === 'function') {
+            return setValue(v => ({ ...v, ...newValue(v) }))
+        }
+        return
+    }];
 }
