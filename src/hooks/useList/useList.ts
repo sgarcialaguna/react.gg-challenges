9a3cb4db912ed788fcca828f05a5bbe7d8787155
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-04 14:52:51 +0200

    Implement useList

--- src/hooks/useList/useList.ts
+++ src/hooks/useList/useList.ts
@@ -1,21 +1,23 @@
 import * as React from "react";
 
-const placeholder = () => { };
+declare type ListFunctions = {
+    set: (l: Array<unknown>) => void
+    push: (value: unknown) => void
+    removeAt: (idx: number) => void
+    insertAt: (idx: number, value: unknown) => void
+    updateAt: (idx: number, value: unknown) => void
+    clear: () => void
+}
 
-export default function useList(defaultList = []) {
+export default function useList(defaultList: Array<unknown> = []): [list: Array<unknown>, ListFunctions] {
     const [list, setList] = React.useState(defaultList);
 
-    const set = placeholder;
-
-    const push = placeholder;
-
-    const removeAt = placeholder;
-
-    const insertAt = placeholder;
-
-    const updateAt = placeholder;
-
-    const clear = placeholder;
-
-    return [list, { set, push, removeAt, insertAt, updateAt, clear }];
+    return [list, {
+        set: setList,
+        push: (value) => setList(l => [...l, value]),
+        removeAt: (idx) => setList(l => [...l.slice(0, idx), ...l.slice(idx + 1)]),
+        insertAt: (idx, value) => setList(l => [...l.slice(0, idx), value, ...l.slice(idx)]),
+        updateAt: (idx, value) => setList(l => [...l.slice(0, idx), value, ...l.slice(idx + 1)]),
+        clear: () => setList([])
+    }];
 }
