2f9316a9b6ef4b14853c299193e0f1c9aa9d235e
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-10 16:11:51 +0200

    Simplify to a local variable instead of a ref

--- src/hooks/useBattery/useBattery.js
+++ src/hooks/useBattery/useBattery.js
@@ -1,10 +1,18 @@
 import { useEffect, useState, useRef } from "react";
 
 export default function useBattery() {
-  const [status, setStatus] = useState({ supported: false, loading: false });
-  const batteryManager = useRef(null);
+  const [status, setStatus] = useState({
+    supported: false,
+    loading: false,
+    level: undefined,
+    charging: undefined,
+    chargingTime: undefined,
+    dischargingTime: undefined,
+  });
 
   useEffect(() => {
+    let batteryManager = null;
+
     async function initBatteryManager() {
       if (!window.navigator.getBattery) {
         return;
@@ -17,47 +25,37 @@ export default function useBattery() {
         chargingTime: undefined,
         dischargingTime: undefined,
       });
-      batteryManager.current = await window.navigator.getBattery();
+      batteryManager = await window.navigator.getBattery();
       updateStatus();
-      batteryManager.current?.addEventListener("levelchange", updateStatus);
-      batteryManager.current?.addEventListener("chargingchange", updateStatus);
-      batteryManager.current?.addEventListener(
-        "chargingtimechange",
-        updateStatus,
-      );
-      batteryManager.current?.addEventListener(
-        "dischargingtimechange",
-        updateStatus,
-      );
+      batteryManager.addEventListener("levelchange", updateStatus);
+      batteryManager.addEventListener("chargingchange", updateStatus);
+      batteryManager.addEventListener("chargingtimechange", updateStatus);
+      batteryManager.addEventListener("dischargingtimechange", updateStatus);
     }
 
     async function updateStatus() {
       setStatus({
         supported: true,
         loading: false,
-        level: batteryManager.current.level,
-        charging: batteryManager.current.charging,
-        chargingTime: batteryManager.current.chargingTime,
-        dischargingTime: batteryManager.current.dischargingTime,
+        level: batteryManager.level,
+        charging: batteryManager.charging,
+        chargingTime: batteryManager.chargingTime,
+        dischargingTime: batteryManager.dischargingTime,
       });
     }
 
     initBatteryManager();
 
     return () => {
-      batteryManager.current?.removeEventListener("levelchange", updateStatus);
-      batteryManager.current?.removeEventListener(
-        "chargingchange",
-        updateStatus,
-      );
-      batteryManager.current?.removeEventListener(
-        "chargingtimechange",
-        updateStatus,
-      );
-      batteryManager.current?.removeEventListener(
-        "dischargingtimechange",
-        updateStatus,
-      );
+      if (batteryManager) {
+        batteryManager.removeEventListener("levelchange", updateStatus);
+        batteryManager.removeEventListener("chargingchange", updateStatus);
+        batteryManager.removeEventListener("chargingtimechange", updateStatus);
+        batteryManager.removeEventListener(
+          "dischargingtimechange",
+          updateStatus,
+        );
+      }
     };
   }, []);
   return status;
