4ceab4ec5df97acd947c1bed0505dbef9053cb43
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-10 13:32:09 +0200

    Solution for useClickAway

--- src/hooks/useClickAway/useClickAway.ts
+++ src/hooks/useClickAway/useClickAway.ts
@@ -1,4 +1,20 @@
-import { experimental_useEffectEvent as useEffectEvent } from "react";
+import { useEffect, experimental_useEffectEvent as useEffectEvent, useRef } from "react";
 
 
-export default function useClickAway() { }
+export default function useClickAway(cb: () => void) {
+    const invokeCallback = useEffectEvent(cb)
+    const ref = useRef<HTMLElement>(null)
+
+    useEffect(() => {
+        function handleClick(event: globalThis.MouseEvent) {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                invokeCallback()
+            }
+        }
+
+        window.addEventListener('mousedown', handleClick)
+        return () => window.removeEventListener('mousedown', handleClick)
+    }, [])
+
+    return ref
+}
