0356bcb4cf44a6587e665cbb9b1d2122ae9224d7
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-10 14:27:55 +0200

    Finish useLogger

--- src/hooks/useLogger/useLogger.ts
+++ src/hooks/useLogger/useLogger.ts
@@ -1,3 +1,18 @@
-import { experimental_useEffectEvent as useEffectEvent } from "react";
+import { useEffect, experimental_useEffectEvent as useEffectEvent, useRef } from "react";
 
-export default function useLogger() { }
+export default function useLogger(name: string, ...args: Array<unknown>) {
+    const mounted = useRef<boolean>(false)
+    const logEvent = useEffectEvent((type: 'mounted' | 'updated' | 'unmounted') => { console.log(`${name} ${type}:`, args) })
+
+    useEffect(() => {
+        if (mounted.current) {
+            logEvent('updated')
+        }
+    }, [args])
+
+    useEffect(() => {
+        logEvent('mounted')
+        mounted.current = true
+        return () => { logEvent('unmounted'); mounted.current = false }
+    }, [])
+}
