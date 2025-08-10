e01512375fcab0f9e25f813bf4e43339626dc240
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-10 12:21:19 +0200

    Implement useMouse

--- src/hooks/useMouse/useMouse.ts
+++ src/hooks/useMouse/useMouse.ts
@@ -1,4 +1,5 @@
-import { useRef, useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
+
 
 export default function useMouse() {
     const [state, setState] = useState({
@@ -10,7 +11,21 @@ export default function useMouse() {
         elementPositionY: 0
     });
 
-    const ref = useRef(null);
+    const ref = useRef<HTMLElement>(null);
+
+    useLayoutEffect(() => {
+        const updateState = (event: globalThis.MouseEvent) => {
+            const rect = ref.current?.getBoundingClientRect()
+            const elementPositionX = rect ? rect.x + window.scrollX : 0
+            const elementPositionY = rect ? rect.y + window.scrollY : 0
+            const elementX = rect ? event.pageX - elementPositionX : 0
+            const elementY = rect ? event.pageY - elementPositionY : 0
+            setState({ x: event.clientX, y: event.clientY, elementX, elementY, elementPositionX, elementPositionY })
+        }
+
+        document.addEventListener('mousemove', updateState)
+        return () => document.removeEventListener("mousemove", updateState)
+    }, [])
 
     return [state, ref];
 }
