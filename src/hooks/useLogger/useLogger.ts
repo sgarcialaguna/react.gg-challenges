9677a893412a93a67a258a7c6c06e59b2c61c776
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-10 14:30:53 +0200

    Implement useLogger

--- src/hooks/useLogger/useLogger.ts
+++ src/hooks/useLogger/useLogger.ts
@@ -4,11 +4,13 @@ export default function useLogger(name: string, ...args: Array<unknown>) {
     const mounted = useRef<boolean>(false)
     const logEvent = useEffectEvent((type: 'mounted' | 'updated' | 'unmounted') => { console.log(`${name} ${type}:`, args) })
 
+    // Effects are evaluated in the order they are declared, so on initial render, mounted.current is false,
+    // then it is set to true
     useEffect(() => {
         if (mounted.current) {
             logEvent('updated')
         }
-    }, [args])
+    })
 
     useEffect(() => {
         logEvent('mounted')
