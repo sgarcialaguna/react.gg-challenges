5dbb22aac66d84d7aa28869b7ab5c49474d0c72d
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-11 11:48:19 +0200

    Complete useGeolocation

--- src/hooks/useGeolocation/useGeolocation.ts
+++ src/hooks/useGeolocation/useGeolocation.ts
@@ -1,7 +1,20 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 
-export default function useGeolocation(options = {}) {
-    const [state, setState] = React.useState({
+declare type State = {
+    loading: boolean;
+    accuracy: number | null;
+    altitude: number | null;
+    altitudeAccuracy: number | null;
+    heading: number | null;
+    latitude: number | null;
+    longitude: number | null;
+    speed: number | null;
+    timestamp: number | null;
+    error: unknown;
+};
+
+export default function useGeolocation(options: PositionOptions = {}) {
+    const [state, setState] = useState<State>({
         loading: true,
         accuracy: null,
         altitude: null,
@@ -11,8 +24,36 @@ export default function useGeolocation(options = {}) {
         longitude: null,
         speed: null,
         timestamp: null,
-        error: null
+        error: null,
     });
+
+    useEffect(() => {
+        const successCallback = (p: GeolocationPosition) => {
+            setState({
+                loading: false,
+                error: null,
+                accuracy: p.coords.accuracy,
+                altitude: p.coords.altitude,
+                altitudeAccuracy: p.coords.altitudeAccuracy,
+                timestamp: p.timestamp,
+                heading: p.coords.heading,
+                latitude: p.coords.latitude,
+                longitude: p.coords.longitude,
+                speed: p.coords.speed,
+            });
+        }
+
+        const errorCallback = (error: GeolocationPositionError) => setState((s) => ({ ...s, error }))
+
+        navigator.geolocation.getCurrentPosition(
+            successCallback,
+            errorCallback,
+            options,
+        );
+        const id = navigator.geolocation.watchPosition(successCallback, errorCallback, options)
+
+        return () => navigator.geolocation.clearWatch(id)
+    }, [options]);
 
     return state;
 }
