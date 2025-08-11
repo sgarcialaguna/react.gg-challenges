64e3167778e20ef4851e61651a04b3eb11d991ab
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-11 04:40:27 +0200

    Implement usePageLeave

--- src/hooks/usePageLeave/usePageLeave.ts
+++ src/hooks/usePageLeave/usePageLeave.ts
@@ -1,3 +1,14 @@
-import { experimental_useEffectEvent } from "react";
+import { experimental_useEffectEvent as useEffectEvent, useEffect } from "react";
 
-export default function usePageLeave(cb) { }
+export default function usePageLeave(cb: () => void) {
+    const invokeCallback = useEffectEvent((event: MouseEvent) => {
+        if (!event.relatedTarget) {
+            cb()
+        }
+    })
+
+    useEffect(() => {
+        window.document.addEventListener('mouseout', invokeCallback)
+        return () => window.document.removeEventListener('mouseout', invokeCallback);
+    }, [])
+}
