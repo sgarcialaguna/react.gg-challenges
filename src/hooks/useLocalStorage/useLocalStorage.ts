d3bcab126692e3aee061c5b9005217c18875c3ba
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-11 13:41:00 +0200

    Complete useLocalStorage

--- src/hooks/useLocalStorage/useLocalStorage.ts
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -1,40 +1,56 @@
-import * as React from "react";
+import { useEffect, useSyncExternalStore } from "react";
 
-const dispatchStorageEvent = (key, newValue) => {
+const dispatchStorageEvent = (key: string, newValue: string | null) => {
     window.dispatchEvent(new StorageEvent("storage", { key, newValue }));
 };
 
-const setItem = (key, value) => {
+const setItem = (key: string, value: string) => {
     const stringifiedValue = JSON.stringify(value);
     window.localStorage.setItem(key, stringifiedValue);
     dispatchStorageEvent(key, stringifiedValue);
 };
 
-const removeItem = (key) => {
+const removeItem = (key: string) => {
     window.localStorage.removeItem(key);
     dispatchStorageEvent(key, null);
 };
 
-const getItem = (key) => {
+const getItem = (key: string) => {
     return window.localStorage.getItem(key);
 };
 
-const subscribe = () => { };
+const subscribe = (callback: () => void) => {
+    window.addEventListener("storage", callback);
+    return () => window.removeEventListener("storage", callback);
+};
 
 const getServerSnapshot = () => {
     throw Error("useLocalStorage is a client-only hook");
 };
 
-export default function useLocalStorage(key, initialValue) {
-    const getSnapshot = () => { };
+export default function useLocalStorage(key: string, initialValue: string) {
+    const getSnapshot = () => getItem(key);
 
-    const store = React.useSyncExternalStore(
-        subscribe,
-        getSnapshot,
-        getServerSnapshot
-    );
+    useEffect(() => {
+        if (!initialValue) {
+            return;
+        }
+        if (getItem(key)) {
+            return;
+        }
+        setItem(key, initialValue);
+    }, [initialValue, key]);
 
-    const setState = () => { };
+    const store = useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+
+    const setState = (value: string | ((oldValue: string) => string)) => {
+        if (typeof value === "function") {
+            setItem(key, value(JSON.parse(getItem(key)!)))
+        }
+        else {
+            setItem(key, value);
+        }
+    };
 
     return [store ? JSON.parse(store) : initialValue, setState];
 }
