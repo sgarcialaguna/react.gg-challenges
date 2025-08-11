a839b7c0357b7d1ca24b40da1adc5b41db9e8805
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-11 11:16:59 +0200

    Complete useCountdown

--- src/hooks/useCountdown/useCountdown.ts
+++ src/hooks/useCountdown/useCountdown.ts
@@ -1,8 +1,30 @@
-import { experimental_useEffectEvent, useState } from "react";
+import { useEffect, experimental_useEffectEvent as useEffectEvent, useRef, useState } from "react";
 
 
-export default function useCountdown(endTime: unknown, options: unknown) {
-    const [count, setCount] = useState(null);
+export default function useCountdown(endTime: number, options: { interval: number, onTick: () => void, onComplete: (time: number) => void }) {
+    const { interval, onTick, onComplete } = options
+    const [count, setCount] = useState(Math.round((endTime - Date.now()) / interval));
+    const id = useRef<number>(null)
+
+    const handleInterval = useEffectEvent(() => {
+        if (count > 0) {
+            setCount(c => c - 1)
+            onTick()
+        }
+        if (count === 0) {
+            window.clearInterval(id.current!)
+            id.current = null
+            callOnComplete(Date.now())
+        }
+    })
+
+    const callOnComplete = useEffectEvent(onComplete)
+
+    useEffect(() => {
+        id.current = window.setInterval(handleInterval, interval)
+
+        return () => { window.clearInterval(id.current!) }
+    }, [interval])
 
     return count;
 }
