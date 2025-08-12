3141da6759ce4dbb0b115fb7a17bd1a863d7e206
Santiago Garcia <santiago.garcia@posteo.de>
2025-08-12 07:00:59 +0200

    Complete useSessionStorage

--- src/hooks/useSessionStorage/useSessionStorage.ts
+++ src/hooks/useSessionStorage/useSessionStorage.ts
@@ -1,40 +1,62 @@
-import * as React from "react";
+import { useEffect, useSyncExternalStore } from "react";
 
-const dispatchStorageEvent = (key, newValue) => {
+const dispatchStorageEvent = (key: string, newValue: string | null) => {
     window.dispatchEvent(new StorageEvent("storage", { key, newValue }));
 };
 
-const setItem = (key, value) => {
+const setItem = (key: string, value: unknown) => {
     const stringifiedValue = JSON.stringify(value);
     window.sessionStorage.setItem(key, stringifiedValue);
     dispatchStorageEvent(key, stringifiedValue);
 };
 
-const removeItem = (key) => {
+const removeItem = (key: string) => {
     window.sessionStorage.removeItem(key);
     dispatchStorageEvent(key, null);
 };
 
-const getItem = (key) => {
+const getItem = (key: string) => {
     return window.sessionStorage.getItem(key);
 };
 
-const subscribe = () => { };
+const subscribe = (callback: () => void) => {
+    window.addEventListener('storage', callback)
+    return () => window.removeEventListener('storage', callback)
+};
 
 const getServerSnapshot = () => {
     throw Error("useSessionStorage is a client-only hook");
 };
 
-export default function useSessionStorage(key, initialValue) {
-    const getSnapshot = () => { };
+export default function useSessionStorage(key: string, initialValue: unknown) {
+    const getSnapshot = () => getItem(key)
 
-    const store = React.useSyncExternalStore(
+    useEffect(() => {
+        if (!initialValue) {
+            return;
+        }
+        if (getItem(key)) {
+            return;
+        }
+        setItem(key, initialValue);
+    }, [initialValue, key]);
+
+
+    const store = useSyncExternalStore(
         subscribe,
         getSnapshot,
         getServerSnapshot
     );
 
-    const setState = () => { };
+    const setState = (value: string | ((oldValue: string) => string)) => {
+        if (typeof value === "function") {
+            setItem(key, value(JSON.parse(getItem(key)!)))
+        }
+        else {
+            setItem(key, value);
+        }
+    };
+
 
     return [store ? JSON.parse(store) : initialValue, setState];
 }
